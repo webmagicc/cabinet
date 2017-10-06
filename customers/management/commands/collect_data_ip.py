14da479b621130fab5b4056886ad838fe7638de4
Vladimir Dominion <webmagic@ukr.net>
2017-10-06 10:39:16 +0300

    collect ip

---
+++ customers/management/commands/collect_data_ip.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand, CommandError
+from pymongo import MongoClient
+
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+    	pass
---
+++ customers/management/commands/create_report_ip.py
@@ -0,0 +1,26 @@
+from django.core.management.base import BaseCommand, CommandError
+from pymongo import MongoClient
+from customers.models import Resouce
+import re
+
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        remote_client = MongoClient('mongodb://adv2:<ju,kfujq@5.45.115.126:27017/adv')
+        remote_db = remote_client['adv']
+        local_client = MongoClient('mongodb://localhost:27017/')
+        local_db = local_client['cabinet']
+
+        for s in Resouce.objects.prefetch_related('watch').all():
+        	print(s.domain)
+        	for w in s.watch.all():
+        		print(w.name)
+        		if w.name == 'Google':
+        			google_ips = []
+        			regx = re.compile("gclid=", re.IGNORECASE)
+        			for i in remote_db.hits.find({"$and":[{"domain":s.domain},{ "href": regx }]}):
+        				print(i)
+        		if w.name == 'Yandex':
+        			pass
